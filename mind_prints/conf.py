764334f3f9939fab5fcf2323bc8c266abd179f33
aaron <aaron.peters@zolontech.com>
2025-07-31 10:24:57 -0400

    Fixed html Contents problem, and got references working.

--- mind_prints/conf.py
+++ mind_prints/conf.py
@@ -1,3 +1,5 @@
+# sphinx-build --builder html -write-all --fresh-env mind_prints docs
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -15,12 +17,12 @@ release = '0.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import sphinx_rtd_theme
 
-extensions = ['sphinx_rtd_theme',]
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
